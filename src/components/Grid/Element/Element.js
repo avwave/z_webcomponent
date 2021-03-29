689dbd49babd5922b122f5fd8142410d83409571
Ray Arvin Rimorin <ray@fbmsoftware.com>
2021-03-29 14:36:20 +0800

    Grid Element component

---
+++ src/components/Grid/Element/Element.js
@@ -0,0 +1,51 @@
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+import * as Styles from "./styles";
+import { LinearProgress } from "@material-ui/core";
+
+const StyledContainer = styled.div``;
+const StyledElement = styled.div`
+  ${Styles.base};
+  ${(props) => Styles.variant[props.variant]};
+  padding-bottom: ${(props) => (props.hasProgress ? "0px" : "4px")};
+`;
+const StyledTitle = styled.div`
+  font-weight: bold;
+  line-height: 1.5;
+`;
+const StyledSubtitle = styled.div`
+  font-weight: normal;
+  font-size: smaller;
+`;
+
+export default function Element({
+  title,
+  subtitle,
+  onClick,
+  variant,
+  hasProgress,
+}) {
+  return (
+    <StyledContainer>
+      <StyledElement
+        variant={variant}
+        onClick={onClick}
+        hasProgress={hasProgress}
+      >
+        <StyledTitle>{title}</StyledTitle>
+        <StyledSubtitle>{subtitle}</StyledSubtitle>
+      </StyledElement>
+      {hasProgress ? <LinearProgress /> : <></>}
+    </StyledContainer>
+  );
+}
+
+Element.propTypes = {
+  onClick: PropTypes.func,
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  variant: PropTypes.string,
+  hasProgress: PropTypes.bool,
+};
---
+++ src/components/Grid/Element/index.stories.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { action, actions } from "@storybook/addon-actions";
+import { Element } from ".";
+
+const ElementStory = {
+  component: Element,
+  title: "Grid/Element",
+};
+
+export default ElementStory;
+
+const DefaultStory = ({ ...args }) => (
+  <Element {...args} onClick={action("Element onClick")} />
+);
+export const Default = DefaultStory.bind({});
+Default.args = {
+  title: "Element1",
+  variant: "default",
+};
+
+export const Subtitle = DefaultStory.bind({});
+Subtitle.args = {
+  title: "Element1",
+  subtitle: "subtitle",
+  variant: "default",
+};
+
+export const Header = DefaultStory.bind({});
+Header.args = {
+  title: "Element1",
+  variant: "header",
+};
+
+export const Available = DefaultStory.bind({});
+Available.args = {
+  title: "Element1",
+  variant: "available",
+};
+
+export const Closed = DefaultStory.bind({});
+Closed.args = {
+  title: "Element1",
+  variant: "closed",
+};
+
+export const Scheduled = DefaultStory.bind({});
+Scheduled.args = {
+  title: "Element1",
+  variant: "scheduled",
+};
+
+export const Progress = DefaultStory.bind({});
+Progress.args = {
+  title: "Element1",
+  variant: "default",
+  hasProgress: true,
+};
