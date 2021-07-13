88ee260de50dee3a24c7055872ad248424f957b4
Ray Arvin Rimorin <rayarvin@gmail.com>
2021-07-13 20:59:56 +0800

    ZI-8935 storyboard bug: material-ui context handled by default in storybook 6.3.x

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,7 @@
 import "../src/reset.css";
 import "../src/index.css";
-import { withReactContext } from "storybook-react-context";
-export const decorators = [withReactContext];
+// import { withReactContext } from "storybook-react-context";
+// export const decorators = [withReactContext];
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 };
