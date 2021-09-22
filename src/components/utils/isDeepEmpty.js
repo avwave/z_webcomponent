14020ea54215244891278ec4e89524aecd342553
Ray Arvin Rimorin <rayarvin@gmail.com>
2021-09-22 17:19:57 +0800

    zi-9685 separate date objects to be parsed as deeplyempty

--- src/components/utils/isDeepEmpty.js
+++ src/components/utils/isDeepEmpty.js
@@ -1,16 +1,22 @@
 import { isEmpty } from "lodash";
 
-export const isDeeplyEmpty = item => {
-  if(typeof item === 'boolean') return false;
-  else if(typeof item === 'number') return false;
-  else if(typeof item === 'object') {
-    return Object.keys(item).every(k => {
-      if(['object', 'boolean', 'number'].includes(typeof item[k])) {
-        if (item[k] === null || item[k] === undefined) return true;
+export const isDeeplyEmpty = (item) => {
+  if (typeof item === "boolean") {
+    return false;
+  } else if (typeof item === "number") {
+    return false;
+  } else if (typeof item === "object") {
+    return Object.keys(item).every((k) => {
+      if (Object.prototype.toString.call(item[k]) === "[object Date]") {
+        return false
+      }
+      else if (["object", "boolean", "number"].includes(typeof item[k])) {
+        if (item[k] === null || item[k] === undefined) {
+          return true;
+        } 
         return isDeeplyEmpty(item[k]);
       }
-      return isEmpty(item[k]);
-    })
+    });
   }
   return !item;
 };
