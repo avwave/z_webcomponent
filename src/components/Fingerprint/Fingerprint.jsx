4dfe12e658585297894991a969ce62b33af9d703
Ray Arvin Rimorin <rayarvin@gmail.com>
2022-03-03 11:03:28 +0800

    ZI-11077 implement fingerprintjs visitor id as component optional as GUID if browser fingerprinting not enough

--- src/components/Fingerprint/Fingerprint.jsx
+++ src/components/Fingerprint/Fingerprint.jsx
@@ -1,6 +1,9 @@
 import { makeStyles } from '@material-ui/core';
 import React, { useEffect, useMemo, useState } from 'react';
 import FingerprintJS from '@fingerprintjs/fingerprintjs'
+import { get, set } from "idb-keyval"
+import { v4 as uuidv4 } from "uuid";
+import isEmpty from 'lodash.isempty';
 
 const useStyles = makeStyles((theme) => {
   return {}
@@ -8,10 +11,14 @@ const useStyles = makeStyles((theme) => {
 
 let fpPromise = null
 
-const registerFingerprint = () => {
+const registerFingerprint = async () => {
   fpPromise = FingerprintJS.load({
     monitoring: false,
   })
+  const fp = await get('fingerprint')
+  if (isEmpty(fp)) {
+    await set('fingerprint', uuidv4())
+  }
 }
 
 const useFingerprint = async () => {
@@ -20,7 +27,11 @@ const useFingerprint = async () => {
   return fpData?.visitorId
 }
 
-const Fingerprint = ({ onFingerprint = () => { } }) => {
+const deleteFingerprint = async () => {
+  await set('fingerprint', null)
+}
+
+const Fingerprint = ({ guid=false, onFingerprint = () => { } }) => {
   const classes = useStyles()
 
   const [fp, setFp] = useState();
@@ -30,16 +41,17 @@ const Fingerprint = ({ onFingerprint = () => { } }) => {
     const fetchFP = async () => {
       const fp = await fpPromise
       const fpData = await fp.get()
+      const guidFp = await get('fingerprint')
       setFp(fpData);
-      onFingerprint(fpData?.visitorId)
+      onFingerprint(guid ? guidFp : fpData?.visitorId)
     }
 
     fetchFP()
-  }, [onFingerprint]);
+  }, [guid, onFingerprint]);
 
   return (
     <></>
   )
 }
 
-export { registerFingerprint, useFingerprint, Fingerprint }
+export { deleteFingerprint, registerFingerprint, useFingerprint, Fingerprint }
