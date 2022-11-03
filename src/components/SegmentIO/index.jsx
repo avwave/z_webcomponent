a570ff34ba7dae2181d4e49a7cb18dc55f1bb36e
Ray Arvin Rimorin <rayarvin@gmail.com>
2022-11-03 18:35:44 +0800

    ZI-12811 integrate-segment-as-components

---
+++ src/components/SegmentIO/index.jsx
@@ -0,0 +1,132 @@
+import { makeStyles } from '@material-ui/core';
+import { AnalyticsBrowser } from '@segment/analytics-next';
+import React, { useCallback, useMemo } from 'react';
+import useLocalStorage from "use-local-storage";
+
+const useStyles = makeStyles((theme) => {
+  return {}
+})
+
+const AnalyticsContext = React.createContext(null)
+
+const AnalyticsProvider = ({ children, writeKey, appIdentifier }) => {
+  const analytics = useMemo(
+    () => {
+      const analytics = { ...AnalyticsBrowser.load({ writeKey }), appIdentifier }
+      return analytics
+    }, [writeKey]
+  );
+
+  return (
+    <AnalyticsContext.Provider value={analytics}>
+      {children}
+    </AnalyticsContext.Provider>
+  )
+}
+
+const COMMONPAYLOAD = {
+  integrations: {
+    All: true,
+  }
+}
+const useAnalytics = () => {
+
+  const [claimed, setClaimed] = useLocalStorage("IDClaimed", null);
+
+
+  const analytics = React.useContext(AnalyticsContext)
+  if (!analytics) {
+    throw new Error('useAnalytics must be used within a AnalyticsProvider')
+  }
+
+  const pageViewed = useCallback(
+    (name) => {
+      analytics?.page(analytics?.appIdentifier, name)
+    },
+    [analytics],
+  );
+
+  const trackEvent = useCallback(
+    async (eventName, properties) => {
+      const aUser = await analytics?.user()
+      const id = await (aUser)?.id()
+      const aId = await (aUser)?.anonymousId()
+      const identifiers = (!!id && aId === id) ? { anonymousTempId: aId } : { id }
+      analytics?.track(eventName, { ...identifiers, ...properties, appIdentifier: analytics?.appIdentifier }, COMMONPAYLOAD)
+    },
+    [analytics],
+  );
+
+  const mergeIdentity = useCallback(
+    async (userId) => {
+      if (!claimed && !!userId) {
+        setClaimed(true)
+        await analytics?.alias(userId, COMMONPAYLOAD)
+      }
+    },
+    [analytics, claimed, setClaimed],
+  )
+
+  const identifyUser = useCallback(
+    async (id, traits) => {
+      const identity = id || await (await analytics?.user())?.anonymousId()
+      const identifiers = !!id ? { id } : { anonymousTempId: identity }
+      await analytics?.identify(identity, { ...identifiers, ...traits, appIdentifier: analytics?.appIdentifier }, COMMONPAYLOAD)
+      // await mergeIdentity(id)
+    },
+    [analytics, mergeIdentity],
+  );
+
+  const getAnonymousId = useCallback(
+    async () => {
+      const user = await analytics?.user()
+      return user?.anonymousId()
+    },
+    [analytics],
+  );
+
+  const checkIsIdentified = useCallback(
+    async () => {
+      const user = await analytics?.user()
+
+      const id = await user?.id()
+      return id
+    },
+    [analytics],
+  );
+
+  const reset = useCallback(
+    async () => {
+      const user = await analytics?.user()
+      await user?.id(null)
+      await user?.traits(null)
+      await identifyUser(null)
+    },
+    [analytics, identifyUser],
+  );
+
+  const fullReset = useCallback(
+    async () => {
+      await analytics?.reset()
+      await setClaimed(false)
+      await reset()
+    },
+    [analytics, reset, setClaimed],
+  );
+
+  return {
+    getAnonymousId,
+    reset,
+    fullReset,
+    analytics,
+    pageViewed,
+    trackEvent,
+    identifyUser,
+    mergeIdentity,
+    checkIsIdentified
+  }
+}
+
+export { AnalyticsProvider, useAnalytics };
+
+
---
+++ src/components/SegmentIO/index.stories.js
@@ -0,0 +1,248 @@
+import React, { useCallback, useEffect } from "react";
+import { AnalyticsProvider, useAnalytics } from ".";
+import { useState } from '../DataGrid2/stateref';
+import ReactJsonView from 'react-json-view';
+import { withReactContext } from "storybook-react-context";
+import { Button } from "@material-ui/core";
+
+const SegmentIOStory = {
+
+  title: "SegmentIO",
+  decorators: [
+    withReactContext(),
+    (Story) => (
+      <AnalyticsProvider writeKey='yalj3MiSxgrMHEgmFFGMuQa52N9y5S3D' appIdentifier='STORYBOOK'>
+        <Story />
+      </AnalyticsProvider>
+    ),
+  ],
+};
+
+export default SegmentIOStory;
+
+const DefaultStory = ({ ...args }) => {
+  const {
+    getAnonymousId
+  } = useAnalytics()
+
+  const [ident, setIdent] = useState();
+
+  const identify = useCallback(
+    async () => {
+      const i = await getAnonymousId()
+      console.log('index.stories.js (31) # i', i);
+      setIdent(i)
+    },
+    [getAnonymousId],
+  );
+  useEffect(() => {
+    identify()
+  }, []);
+
+  return (
+    <ReactJsonView src={{ ident }} />
+  )
+}
+
+export const Default = DefaultStory.bind({});
+Default.args = {
+
+}
+
+
+const IdentifyAnonStory = ({ ...args }) => {
+  const {
+    identifyUser
+  } = useAnalytics()
+
+  const [ident, setIdent] = useState();
+
+  const identify = useCallback(
+    async () => {
+      const i = await identifyUser()
+      console.log('index.stories.js (31) # i', i);
+      setIdent(i)
+    },
+    [identifyUser],
+  );
+  return (
+    <>
+      <ReactJsonView src={{ ident }} />
+      <Button onClick={() => identify()}>Identify</Button>
+    </>
+
+  )
+}
+
+export const IdentifyAnon = IdentifyAnonStory.bind({});
+
+const MergeIdentityStory = ({ ...args }) => {
+  const {
+    mergeIdentity,
+    identifyUser,
+    getAnonymousId
+  } = useAnalytics()
+  const [ident, setIdent] = useState();
+
+  const merge = useCallback(
+    async (id) => {
+      await identifyUser(id)
+      const i = await getAnonymousId()
+      console.log('index.stories.js (31) # i', i);
+      setIdent(i)
+    },
+    [getAnonymousId, identifyUser, mergeIdentity],
+  );
+
+  return (
+    <>
+      <ReactJsonView src={{ ident }} />
+      <Button onClick={() => merge('1647')}>Merge '1647'</Button>
+      <Button onClick={() => merge('1600')}>Merge '1600'</Button>
+    </>
+  )
+}
+
+export const MergeIdentity = MergeIdentityStory.bind({});
+
+const ResetStory = ({ ...args }) => {
+  const {
+    reset,
+    fullReset,
+    getAnonymousId
+  } = useAnalytics()
+  const [ident, setIdent] = useState();
+  const [newIdent, setNewIdent] = useState();
+
+  const getAnon = useCallback(
+    async () => {
+      const i = await getAnonymousId()
+      setIdent(i)
+    },
+    [getAnonymousId],
+  );
+
+  useEffect(() => {
+    getAnon()
+  
+    return () => {
+      
+    }
+  }, []);
+
+  const doReset = useCallback(
+    async () => {
+      await reset()
+      const i = await getAnonymousId()
+      setNewIdent(i)
+    },
+    [getAnonymousId, reset],
+  );
+  const doFullReset = useCallback(
+    async () => {
+      await fullReset()
+      const i = await getAnonymousId()
+      setNewIdent(i)
+    },
+    [fullReset, getAnonymousId],
+  );
+
+  return (
+    <>
+      <ReactJsonView src={{ ident, newIdent }} />
+      <Button onClick={() => doReset()}>Reset</Button>
+      <Button onClick={() => doFullReset()}>Reset Everything (unclaim)</Button>
+    </>
+  )
+}
+
+export const Reset = ResetStory.bind({});
+
+
+const TrackEventStory = ({ ...args }) => {
+  const {
+    trackEvent,
+    reset,
+    checkIsIdentified
+  } = useAnalytics()
+
+  const track = useCallback(
+    () => {
+      trackEvent('TEST_EVENT', { payload: 'test' })
+    },
+    [trackEvent],
+  );
+
+  const [isIdent, setIsIdent] = useState();
+
+  const isIdentified = useCallback(
+    async () => {
+      const isid = await checkIsIdentified()
+      setIsIdent(isid)
+    },
+    [checkIsIdentified],
+  );
+
+  useEffect(() => {
+    isIdentified()
+  }, []);
+
+  return (
+    <>
+    <ReactJsonView src={{ isIdent }} />
+    <Button onClick={() => track()}>Track Button Event{!isIdent && ' Anonymously'}</Button>
+    <Button disabled={!isIdent} onClick={async() => {
+      await reset()
+      isIdentified()
+    }}>Reset</Button>
+    </>
+  )
+}
+
+export const TrackEvent = TrackEventStory.bind({});
+
+const TrackPageStory = ({ ...args }) => {
+  const {
+    pageViewed,
+    reset,
+    fullReset,
+    checkIsIdentified
+  } = useAnalytics()
+
+  const [isIdent, setIsIdent] = useState();
+
+  const isIdentified = useCallback(
+    async () => {
+      const isid = await checkIsIdentified()
+      setIsIdent(isid)
+    },
+    [checkIsIdentified],
+  );
+
+  useEffect(() => {
+    isIdentified()
+  }, []);
+
+  useEffect(() => {
+    pageViewed('Trackpagestory')
+  
+  }, []);
+
+  return (
+    <>
+    <ReactJsonView src={{ isIdent }} />
+    <Button disabled={!isIdent} onClick={async() => {
+      await reset()
+      isIdentified()
+    }}>Reset</Button>
+    <Button disabled={!isIdent} onClick={async() => {
+      await fullReset()
+      isIdentified()
+    }}>Reset Everything (unclaim)</Button>
+    </>
+  )
+}
+
+export const TrackPage = TrackPageStory.bind({});
+
+
