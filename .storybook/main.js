82697ec93d2421e29bcfc470c70df0a248831a22
Ray Arvin Rimorin <rayarvin@gmail.com>
2022-12-08 20:40:15 +0800

    prepare for codemod

--- .storybook/main.js
+++ .storybook/main.js
@@ -12,6 +12,7 @@ module.exports = {
     "../src/components/**/*.stories.@(js|jsx|ts|tsx)",
   ],
   "addons": [
+    "storybook-addon-material-ui",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app",
