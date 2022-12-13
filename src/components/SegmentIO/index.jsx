edbb1b1ee66b412ef25f0893f3241b3dc6bebb60
Ray Arvin Rimorin <rayarvin@gmail.com>
2022-12-13 14:25:06 +0800

    ZI-13041 try: locking context until loaded

--- src/components/SegmentIO/index.jsx
+++ src/components/SegmentIO/index.jsx
@@ -1,4 +1,4 @@
-import { makeStyles } from '@material-ui/core';
+import { CircularProgress, makeStyles } from '@material-ui/core';
 import { AnalyticsBrowser } from '@segment/analytics-next';
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import useLocalStorage from "use-local-storage";
@@ -44,7 +44,9 @@ const AnalyticsProvider = ({ children, writeKey, appIdentifier }) => {
     }, [setupAnalytics, writeKey]
   );
 
-
+  if(loading) {
+    return <CircularProgress/>
+  }
   return (
     <AnalyticsContext.Provider value={{ loading, analytics: analyticsData, appIdentifier }}>
       {children}
@@ -54,21 +56,29 @@ const AnalyticsProvider = ({ children, writeKey, appIdentifier }) => {
 
 const useAnalytics = () => {
 
+  const { loading, analytics, appIdentifier } = React.useContext(AnalyticsContext)
+  const [analyticsLib, setAnalyticsLib] = useState(null);
+  useEffect(
+    () => {
+      if (!loading) {
+        setAnalyticsLib(analytics)
+      }
+    }, [loading]
+  );
+
   const pageViewed = useCallback(
     (name, properties) => {
-      const { loading, analytics, appIdentifier } = React.useContext(AnalyticsContext)
-      analytics?.page(appIdentifier, name, properties, COMMONPAYLOAD)
+      analyticsLib?.page(appIdentifier, name, properties, COMMONPAYLOAD)
     },
-    [],
+    [analyticsLib],
   );
 
   const trackEvent = useCallback(
     async (eventName, properties) => {
-      const { loading, analytics, appIdentifier } = React.useContext(AnalyticsContext)
-      if (!analytics) {
+      if (!analyticsLib) {
         return  //silent error, possible show disable tracking/adblock message
       }
-      const aUser = await analytics?.user()
+      const aUser = await analyticsLib?.user()
       const id = await (aUser)?.id()
       const aId = await (aUser)?.anonymousId()
       const identifiers = {
@@ -82,48 +92,45 @@ const useAnalytics = () => {
         appIdentifier: appIdentifier
       })
       // console.log("SEG: 📢[index.jsx:56]: payload: ", payload);
-      await analytics?.track(eventName, payload, COMMONPAYLOAD)
+      await analyticsLib?.track(eventName, payload, COMMONPAYLOAD)
     },
-    [],
+    [analyticsLib],
   );
   const aliasToV1 = useCallback(
     async (userId, forceClaim = false) => {
-      const { loading, analytics, appIdentifier } = React.useContext(AnalyticsContext)
-      if (!analytics) {
+      if (!analyticsLib) {
         return  //silent error, possible show disable tracking/adblock message
       }
-      const anonId = await (await analytics?.user())?.anonymousId()
+      const anonId = await (await analyticsLib?.user())?.anonymousId()
       if (forceClaim) {
-        await analytics?.alias(userId, COMMONPAYLOAD)
+        await analyticsLib?.alias(userId, COMMONPAYLOAD)
         return
       }
       if (!(sessionStorage.getItem("IDClaimed") === 'yes') && !!userId) {
         sessionStorage.setItem("IDClaimed", 'yes')
-        await analytics?.alias(userId, anonId, COMMONPAYLOAD)
+        await analyticsLib?.alias(userId, anonId, COMMONPAYLOAD)
       }
     },
-    [],
+    [analyticsLib],
   )
 
   const aliasTo = useCallback(
     async (userId) => {
-      const { loading, analytics, appIdentifier } = React.useContext(AnalyticsContext)
-      if (!analytics) {
+      if (!analyticsLib) {
         return  //silent error, possible show disable tracking/adblock message
       }
-      const anonId = await (await analytics?.user())?.anonymousId()
-      const aliau = await analytics?.alias(userId, anonId, COMMONPAYLOAD)
+      const anonId = await (await analyticsLib?.user())?.anonymousId()
+      const aliau = await analyticsLib?.alias(userId, anonId, COMMONPAYLOAD)
     },
-    [],
+    [analyticsLib],
   )
 
   const identifyUsingIdAndTraitsV1 = useCallback(
     async (id, traits) => {
-      const { loading, analytics, appIdentifier } = React.useContext(AnalyticsContext)
-      if (!analytics) {
+      if (!analyticsLib) {
         return  //silent error, possible show disable tracking/adblock message
       }
-      const anonId = await (await analytics?.user())?.anonymousId()
+      const anonId = await (await analyticsLib?.user())?.anonymousId()
       const identity = id || anonId
       const identifiers = !!id ? { userId: id } : { tempId: identity }
 
@@ -136,21 +143,20 @@ const useAnalytics = () => {
         ...COMMONPAYLOAD,
         anonymousId: anonId
       })
-      await analytics?.identify(identity, payload, options)
+      await analyticsLib?.identify(identity, payload, options)
     },
-    [],
+    [analyticsLib],
   );
 
   const identifyUsingIdAndTraits = useCallback(
     async (id = null, traits) => {
-      const { loading, analytics, appIdentifier } = React.useContext(AnalyticsContext)
-      if (!analytics) {
+      if (!analyticsLib) {
         return  //silent error, possible show disable tracking/adblock message
       }
       let identity = id
       let anonTraits = null
 
-      const anonymousId = await (await analytics?.user())?.anonymousId()
+      const anonymousId = await (await analyticsLib?.user())?.anonymousId()
       const anonId = {
         anonymousId
       }
@@ -173,19 +179,18 @@ const useAnalytics = () => {
         ...anonId
       })
 
-      await analytics?.identify(identity, payload, options)
+      await analyticsLib?.identify(identity, payload, options)
       // console.log("SEG: 📢[index.jsx:117]: identUIdT: ", identUIdT);
     },
-    [],
+    [analyticsLib],
   );
 
   const identifyAnon = useCallback(
     async (traits) => {
-      const { loading, analytics, appIdentifier } = React.useContext(AnalyticsContext)
-      if (!analytics) {
+      if (!analyticsLib) {
         return  //silent error, possible show disable tracking/adblock message
       }
-      const anonId = await (await analytics?.user())?.anonymousId()
+      const anonId = await (await analyticsLib?.user())?.anonymousId()
       const payload = {
         tempId: anonId,
         ...traits,
@@ -196,68 +201,65 @@ const useAnalytics = () => {
       }
       if (!(sessionStorage.getItem("IDClaimed") === 'yes')) {
         sessionStorage.setItem("IDClaimed", 'yes')
-        await analytics?.identify(null, payload, options)
+        await analyticsLib?.identify(null, payload, options)
       }
-    }, []
+    }, [analyticsLib]
   )
 
 
   const getAnonymousId = useCallback(
     async () => {
-      const { loading, analytics, appIdentifier } = React.useContext(AnalyticsContext)
-      if (!analytics) {
+      if (!analyticsLib) {
         return  //silent error, possible show disable tracking/adblock message
       }
       try {
-        const user = await analytics?.user()
+        const user = await analyticsLib?.user()
         return user?.anonymousId()
       } catch (e) {
         return null
       }
     },
-    [],
+    [analyticsLib],
   );
 
   const checkIsIdentified = useCallback(
     async () => {
-      const { loading, analytics, appIdentifier } = React.useContext(AnalyticsContext)
-      if (!analytics) {
+      if (!analyticsLib) {
         return  //silent error, possible show disable tracking/adblock message
       }
-      const user = await analytics?.user()
+      const user = await analyticsLib?.user()
 
       const id = await user?.id()
       return id
     },
-    [],
+    [analyticsLib],
   );
 
   const reset = useCallback(
     async () => {
-      const { loading, analytics, appIdentifier } = React.useContext(AnalyticsContext)
-      const user = await analytics?.user()
+      const user = await analyticsLib?.user()
       await user?.id(null)
       await user?.traits(null)
       // await identifyAnon()
     },
-    [],
+    [analyticsLib, identifyAnon],
   );
 
   const fullReset = useCallback(
     async () => {
-      const { loading, analytics, appIdentifier } = React.useContext(AnalyticsContext)
-      const anReset = await analytics?.reset()
+      const anReset = await analyticsLib?.reset()
       // console.log("SEG: 📢[index.jsx:172]: anReset: ", anReset);
       // await setClaimed(false)
       // sessionStorage.removeItem("IDClaimed")
     },
-    [],
+    [analyticsLib],
   );
 
   return {
     getAnonymousId,
     reset,
     fullReset,
+    analyticsLib,
     pageViewed,
     trackEvent,
     identifyUsingIdAndTraits,
