e73b6b06bdeae3c276efacdfab1e8f61cfde018c
Ray Arvin Rimorin <ray@fbmsoftware.com>
2023-06-27 11:46:35 +0800

    ZI-14135 add manual update location

--- src/components/hooks/useGeolocation.stories.js
+++ src/components/hooks/useGeolocation.stories.js
@@ -1,5 +1,7 @@
+import { Button } from "@mui/material";
 import { useGeolocation } from "./useGeolocation";
 import ReactJson from "react-json-view";
+import { useState } from "react";
 
 const useGeolocationStory = {
   title: "Hooks/useGeolocation",
@@ -24,3 +26,32 @@ export const Default = ({ ...args }) => {
 };
 
 Default.args = {};
+
+const DisablePeriodicUpdates = Default.bind({});
+DisablePeriodicUpdates.args = {
+  watchLocation: false
+};
+
+export const ManualUpdate = ({ ...args }) => {
+  const {
+    dialog,
+    error,
+    isGeolocationEnabled,
+    latLng,
+    fetchNewLocation
+  } = useGeolocation({ watchLocation: false });
+
+  const [location, setLocation] = useState({});
+  return (
+    <>
+      {dialog}
+      <ReactJson src={{ error, location }} />
+      <Button onClick={async () => {
+        const newLoc = await fetchNewLocation()
+        setLocation(newLoc)
+      }}>
+        Update Location
+      </Button>
+    </>
+  )
+}
