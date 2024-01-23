47f8128ad3ad4ca545775db24c2222bb7085d406
Ray Arvin Rimorin <ray@fbmsoftware.com>
2024-01-23 18:03:43 +0800

    ZI-15352  instanceref replaced with tableobject hook

--- src/components/VirtuosoDataGrid/DataGridToolbar.jsx
+++ src/components/VirtuosoDataGrid/DataGridToolbar.jsx
@@ -254,13 +254,13 @@ const DataGridToolbar = ({
         >
           {renderFilterBox}
         </Box>
-        {tableInstanceRef.current && (
+        {tableInstanceRef && (
           <Box sx={{ paddingLeft: 2, minWidth: '250px', width: '250px', display: 'flex', flexDirection: 'row', justifyContent:'flex-end' }}>
-            <MRT_GlobalFilterTextField table={tableInstanceRef.current} />
-            {/* <MRT_ToggleFiltersButton table={tableInstanceRef.current} /> */}
-            <MRT_ToggleDensePaddingButton table={tableInstanceRef.current} />
-            {/* <MRT_ShowHideColumnsButton table={tableInstanceRef.current} /> */}
-            <ColumnListFilterMenu table={tableInstanceRef.current} columns={columns}/>
+            <MRT_GlobalFilterTextField table={tableInstanceRef} />
+            {/* <MRT_ToggleFiltersButton table={tableInstanceRef} /> */}
+            <MRT_ToggleDensePaddingButton table={tableInstanceRef} />
+            {/* <MRT_ShowHideColumnsButton table={tableInstanceRef} /> */}
+            <ColumnListFilterMenu table={tableInstanceRef} columns={columns}/>
             {/* <MRT_FullScreenToggleButton table={tableInstanceRef.current} /> */}
           </Box>
         )}
